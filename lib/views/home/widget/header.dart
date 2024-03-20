0ff6a3330d3fd72bbb7781f226aecfa0c7b95a06
adogbeji <baka_1993@yahoo.co.uk>
2024-03-20 19:21:16 +0000

    Add starting files for course home scren header

---
+++ lib/views/home/widget/header.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Course Home Screen Header'),
+      ),
+    );
+  }
+}
