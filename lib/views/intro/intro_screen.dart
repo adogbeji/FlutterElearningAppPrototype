6c543cbcbacdd01ea8d6d42d661d74358fab0cc7
adogbeji <baka_1993@yahoo.co.uk>
2024-05-02 20:24:46 +0100

    Add more text & button to intro screen

--- lib/views/intro/intro_screen.dart
+++ lib/views/intro/intro_screen.dart
@@ -7,10 +7,36 @@ class IntroScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Image.asset('assets/images/intro/intro.png'),
-          const SizedBox(height: 30,),
-          const Text('Grow your Skills'),
+          const SizedBox(
+            height: 30,
+          ),
+          Text(
+            'Grow your Skills',
+            style: TextStyle(
+              fontSize: 25,
+              color: Colors.grey.shade900,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Choose your favourite course & start learning',
+            style: TextStyle(
+              fontSize: 17,
+              color: Colors.grey.shade500,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Getting Started'),
+          ),
         ],
       ),
     );
