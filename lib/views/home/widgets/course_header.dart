3140c77c3d48749116988d1d84152089ded38b87
adogbeji <baka_1993@yahoo.co.uk>
2024-05-15 12:23:19 +0100

    Start adding notification circle to bell icon in course home screen header

--- lib/views/home/widgets/course_header.dart
+++ lib/views/home/widgets/course_header.dart
@@ -9,40 +9,60 @@ class CourseHeader extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-      const Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Welcome John',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
+        const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Welcome John',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
             ),
-          ),
-          SizedBox(height: 10,),
-          Text(
-            'Let\'s learn something new today!',
-            style: TextStyle(
-              fontSize: 18,
-              color: Colors.white,
+            SizedBox(
+              height: 10,
             ),
-          ),
-        ],
-      ),
-      Row(
-        children: [
-          Container(
-            height: 40,
-            width: 40,
-            decoration: const BoxDecoration(
-              color: Colors.white38,
-              borderRadius: BorderRadius.all(Radius.circular(10),),
+            Text(
+              'Let\'s learn something new today!',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+              ),
             ),
-            child: const Icon(Icons.notifications, color: Colors.white,),
-          ),
-        ],
-      ),
-    ],);
+          ],
+        ),
+        Row(
+          children: [
+            Container(
+              height: 40,
+              width: 40,
+              decoration: const BoxDecoration(
+                color: Colors.white38,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+              ),
+              child: Stack(
+                children: [
+                  const Icon(
+                    Icons.notifications,
+                    color: Colors.white,
+                  ),
+                  Container(
+                    height: 10,
+                    width: 10,
+                    decoration: const BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.all(Radius.circular(10),),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
