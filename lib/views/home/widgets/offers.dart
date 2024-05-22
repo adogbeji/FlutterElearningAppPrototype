8398186a9239467dea8705dbb8157c4de36ac1e1
adogbeji <baka_1993@yahoo.co.uk>
2024-05-22 18:43:21 +0100

    Start displaying carousel slideshow in Offers widget

--- lib/views/home/widgets/offers.dart
+++ lib/views/home/widgets/offers.dart
@@ -13,25 +13,33 @@ class Offers extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 250,
-      child: PageView.builder(
-          itemCount: widgetList.length,
-          itemBuilder: (context, index) {
-            return widgetList[index];
-          }),
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          PageView.builder(
+              itemCount: widgetList.length,
+              itemBuilder: (context, index) {
+                return widgetList[index];
+              }),
+          // ),
 
-      // CAROUSEL
-      // Container(
-      //   child: Row(
-      //     children: [
-      //       for (int index = 0; index < widgetList.length; index++)
-      //     ],
-      //   ),
-      // ),
+          // CAROUSEL
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                for (int index = 0; index < widgetList.length; index++)
+                  createCircle()
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 
   // FUNCTION TO CREATE CAROUSEL DOTS
-  Widget createDot() {
+  Widget createCircle() {
     return AnimatedContainer(
       duration: const Duration(
         milliseconds: 150,
@@ -40,6 +48,9 @@ class Offers extends StatelessWidget {
       height: 8,
       decoration: const BoxDecoration(
         color: Colors.grey,
+        borderRadius: BorderRadius.all(
+          Radius.circular(12),
+        ),
       ),
     );
   }
