71c26b2eee49a67b08811889e187709e5e30c3eb
adogbeji <baka_1993@yahoo.co.uk>
2024-05-23 19:18:07 +0100

    Change position of caousel slideshow dots in Offers widget & create new variable

--- lib/views/home/widgets/offers.dart
+++ lib/views/home/widgets/offers.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class Offers extends StatelessWidget {
   Offers({super.key});
 
+  int _selectedPromo = 0;
+
   List<Image> widgetList = [
     Image.asset('assets/images/offer/offer1.png'),
     Image.asset('assets/images/offer/offer2.png'),
@@ -25,6 +27,7 @@ class Offers extends StatelessWidget {
 
           // CAROUSEL
           Container(
+            margin: const EdgeInsets.only(bottom: 30,),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -44,6 +47,7 @@ class Offers extends StatelessWidget {
       duration: const Duration(
         milliseconds: 150,
       ),
+      margin: const EdgeInsets.symmetric(horizontal: 8,),
       width: 8,
       height: 8,
       decoration: const BoxDecoration(
