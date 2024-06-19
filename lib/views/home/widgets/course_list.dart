d2787994ad22f265fdb1832e15fe30bcac725d2d
adogbeji <baka_1993@yahoo.co.uk>
2024-06-19 18:33:05 +0100

    Start creating function in CourseList to get courses for selected category

--- lib/views/home/widgets/course_list.dart
+++ lib/views/home/widgets/course_list.dart
@@ -1,7 +1,13 @@
+// import 'package:elearning_app_prototype/models/course.dart';
+// import 'package:elearning_app_prototype/notifier/course_category_change_notifier.dart';
 import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
 
 import 'package:elearning_app_prototype/views/home/widgets/course_item.dart';
 import 'package:elearning_app_prototype/data_provider/course_data_provider.dart';
+import 'package:elearning_app_prototype/notifier/course_category_change_notifier.dart';
+import 'package:elearning_app_prototype/models/course.dart';
 
 class CourseList extends StatelessWidget {
   const CourseList({super.key});
@@ -16,5 +22,12 @@ class CourseList extends StatelessWidget {
         return CourseItem(course: course);
       }).toList(),
     );
+  }
+
+  List<Course> getCourseList(BuildContext context) {
+    // Let's get selected category using Provider
+    var category = Provider.of<CourseCategoryChangeNotifier>(context).category;
+
+    return CourseDataProvider.courseList.where((course) => course.courseCategory == category).toList();
   }
 }
