15d102583344cb06ed5cd881f690445ee318be22
adogbeji <baka_1993@yahoo.co.uk>
2024-06-22 18:18:43 +0100

    Add padding to bottom navbar & start adding new method

--- lib/views/home/widgets/bottom_navbar.dart
+++ lib/views/home/widgets/bottom_navbar.dart
@@ -3,17 +3,23 @@ import 'package:flutter/material.dart';
 class BottomNavbar extends StatelessWidget {
   const BottomNavbar({super.key});
 
+  final int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return const BottomAppBar(
       child: Row(
         children: [
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Icon(Icons.home),
-              Text('Home'),
-            ],
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.home),
+                SizedBox(height: 5,),
+                Text('Home', style: TextStyle(fontSize: 13,),),
+              ],
+            ),
           ),
           Text('My Courses'),
           Text('Wishlist'),
@@ -21,5 +27,9 @@ class BottomNavbar extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Color getSelectedColor(int optionIndex) {
+    return (selectedIndex == optionIndex) ? Colors.blue: Colors.grey.shade800;
   }
 }
